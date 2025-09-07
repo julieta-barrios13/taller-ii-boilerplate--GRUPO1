46e5e0442fe4cdbb073b1657e13a8982d235e109
Julieta Lujan Barrios <julilujibarrios@gmail.com>
2025-09-07 13:00:53 -0300

    Acá está la tarea de la ultima clase, clasifiqué las columnas segun los datos de mayor importancia

---
+++ entregas semanales/SEMANA2.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+
+url = "https://dummyjson.com/products"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    productos = data["products"]
+
+    df = pd.DataFrame(productos)[["id", "title", "description", "rating", "price", "category"]]
+
+    # Renombro columnas
+    df = df.rename(columns={
+        "id": "ID",
+        "title": "Nombre del producto",
+        "description": "Descripción",
+        "rating": "Puntaje",
+        "price": "Precio",
+        "category": "Categoría"
+    })
+
+    # muestro las primeras filas
+    print(df.head())
+
+ 
+    df.to_csv("productos_filtrados.csv", index=False, encoding="utf-8-sig")
+    print("Datos guardados en productos_filtrados.csv")
+
+else:
+    print("Error al obtener los datos:", response.status_code)
